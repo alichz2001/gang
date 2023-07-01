85dbf61af3d3531ad2bafaa0e22ac3ff188dced6
alichz <alichz2001@gmail.com>
2023-07-01 16:10:25 +0330

    handle warning.

--- lib/gang.ex
+++ lib/gang.ex
@@ -2,12 +2,12 @@ defmodule Gang do
 
   @callback call(atom(), list(any()), keyword()) :: {:ok, list(any())} | :ok | {:error, any()}
 
-  defmacro __using__(opts) do
+  defmacro __using__(_opts) do
     quote do
       import Gang
 
       @behaviour Gang
     end
   end
-  
+
 end
